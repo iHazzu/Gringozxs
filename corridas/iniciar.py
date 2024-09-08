639a694d48517d9266fac4ea56a89e0508c98484
iHazzu <francisvexu@gmail.com>
2024-09-08 17:05:13 -0300

    obter participantes

--- corridas/iniciar.py
+++ corridas/iniciar.py
@@ -8,7 +8,7 @@ async def iniciar_competicao(itc: Interaction):
     itc, criador_id = await fetch_jog_id(itc)
     if not itc:
         return
-    data = await itc.client.db.get("INSERT INTO corridas(criador_id) VALUES (%s) RETURNING id")
+    data = await itc.client.db.get("INSERT INTO corridas(criador_id) VALUES (%s) RETURNING id", criador_id)
     corrida_id = data[0][0]
     thread = await itc.channel.create_thread(name=f"Corrida {corrida_id}", invitable=True)
     embed = Embed(
